f41db6f85fd38493e611ff03b23a6e3e5ac76b4f
Alinka08 <alinazoldubaeva@gmail.com>
2023-05-22 15:05:19 -0500

    Counter practise

---
+++ src/counter/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./counter.style.css";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const incrementNumber = () => {
+    if (counter < 100) {
+      return setCounter(counter + 10);
+    }
+  };
+
+  const decrementNumber = () => {
+    setCounter(counter - 10);
+  };
+
+  return (
+    <>
+      <button onClick={incrementNumber}>+10</button>
+      <h1> {counter} </h1>
+      <button onClick={decrementNumber}>-10</button>
+      {counter === 100 && <h2>You reach max number</h2>}
+    </>
+  );
+};
+
+export default Counter;
---
+++ src/sign-up form/SignupForm.jsx
@@ -0,0 +1,14 @@
+import "./signupForm.style.css";
+import React from "react";
+
+function Signup() {
+  const defaultValue = {
+    fullName: "",
+    dateofBirth: "",
+    password: "",
+    confirmPassword: "",
+  };
+  return <></>;
+}
+
+export default Signup;
